3456e82e2d51f8bd6c389d6b20ba9434379a4e78
Samuel Díaz Millán <fiticuti@gmail.com>
2022-05-26 15:00:54 +0200

    Creación de Utils y finalizado Loading

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, HostListener, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
+import { Utils } from '../shared/utils';
 
 @Component({
   selector: 'app-home',
@@ -7,22 +8,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  music: HTMLAudioElement = new Audio('../../assets/musics/title-screen.mp3');
-  logoTimeout: NodeJS.Timeout;
-  navigationTimeout: NodeJS.Timeout;
+  private music: HTMLAudioElement = new Audio('../../assets/musics/title-screen.mp3');
+  private logoTimeout: NodeJS.Timeout;
+  private navigationTimeout: NodeJS.Timeout;
 
   constructor(
     private element: ElementRef,
     private render: Renderer2,
-    private route: Router
+    private router: Router
   ) {
     this.music.play();
     this.logoTimeout = setTimeout(() => {
       this.changeLogo();
     }, 3700);
     this.navigationTimeout = setTimeout(() => {
-      this.navigateToDifficulty();
-    }, 16500);
+      Utils.navigateTo(this.router, 'loading');
+    }, 16000);
   }
 
   ngOnInit(): void {
@@ -36,35 +37,8 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   @HostListener('window:resize') calculateSizes() {
-    this.calculateBackgroundSize();
-    this.calculateTextSize();
-  }
-
-  calculateBackgroundSize() {
-    const home = this.element.nativeElement.querySelector('.home');
-    const width = window.innerWidth / 1234;
-    const height = window.innerHeight / 1080;
-    if (width > height) {
-      home.style.width = '114.26vh';
-      home.style.height = '100vh';
-    }
-    else {
-      home.style.width = '100vw';
-      home.style.height = '87.52vw';
-    }
-  }
-
-  calculateTextSize() {
-    const html = document.querySelector('html');
-    if (html) {
-      let size: number = 16;
-      const width = window.innerWidth / 1234 * size;
-      const height = window.innerHeight / 1080 * size;
-      width > height
-      ? size = height
-      : size = width;
-      html.style.fontSize = size.toString() + 'px';
-    }
+    Utils.calculateBackgroundSize(this.element, '.home');
+    Utils.calculateTextSize();
   }
 
   changeLogo() {
@@ -72,9 +46,5 @@ export class HomeComponent implements OnInit, OnDestroy {
       this.render.setStyle(letter, 'display', 'none');
     }
     this.render.setStyle(this.element.nativeElement.querySelector('.home-content-logo'), 'display', 'block');
-  }
-
-  navigateToDifficulty() {
-    this.route.navigate(['loading']);
   }
 }
--- src/app/loading/loading.component.ts
+++ src/app/loading/loading.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Utils } from '../shared/utils';
 
 @Component({
   selector: 'app-loading',
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.scss']
 })
-export class LoadingComponent implements OnInit {
+export class LoadingComponent implements OnInit, OnDestroy {
+  public ready: boolean = false;
+  public loadingSprite: number = 1;
+  public noMouse: boolean = false;
+  private readyTimeout: NodeJS.Timeout;
+  private loadingInterval: NodeJS.Timeout;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private element: ElementRef,
+    private router: Router
+  ) {
+    this.readyTimeout = setTimeout(() => {
+      this.ready = true;
+    }, 3000);
+    this.loadingInterval = setInterval(() => {
+      this.changeLoadingSprite();
+    }, 166);
   }
 
+  ngOnInit(): void {
+    this.calculateSizes();
+  }
+
+  ngOnDestroy(): void {
+      clearTimeout(this.readyTimeout);
+      clearInterval(this.loadingInterval);
+  }
+
+  @HostListener('window:resize') calculateSizes() {
+    Utils.calculateBackgroundSize(this.element, '.loading');
+    Utils.calculateTextSize();
+  }
+
+  @HostListener('window:keypress') navigateTo() {
+    if (this.ready) {
+      document.documentElement.requestFullscreen();
+      Utils.navigateTo(this.router, 'home');
+    }
+  }
+
+  changeLoadingSprite() {
+    this.loadingSprite === 1 ? this.loadingSprite++ : this.loadingSprite--;
+  }
+
+  showNoMouse() {
+    this.noMouse = true;
+  }
 }
